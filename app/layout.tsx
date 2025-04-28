d289d67211ad984f1e48781295211c9306613b3f
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-04-28 15:17:17 -0400

    added dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./(marketing)/providers/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Motion",
@@ -23,11 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en suppressHydrationWarning">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system"
+        enableSystem disableTransitionOnChange 
+        storageKey="motion-theme-2">
+          {children}  
+        </ThemeProvider>
       </body>
     </html>
   );
