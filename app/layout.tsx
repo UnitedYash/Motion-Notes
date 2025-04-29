95d0445ee317fa7b173512307cd64034379c2409
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-04-29 12:31:16 -0400

    added database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./(marketing)/providers/theme-provider";
+import { ThemeProvider } from "../components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,13 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en suppressHydrationWarning">
-      <body
-        className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system"
-        enableSystem disableTransitionOnChange 
-        storageKey="motion-theme-2">
-          {children}  
-        </ThemeProvider>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system"
+          enableSystem disableTransitionOnChange 
+          storageKey="motion-theme-2">
+            {children}  
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
