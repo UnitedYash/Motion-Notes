21dc36a1f21b68e0f67419412d50a03b4defeb30
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-01 14:01:11 -0400

    added dark image

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
 const DocumentsPage = () => {
     return ( 
-        <div>
-            This is a protected page
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+                src="/empty.png"
+                height="300"
+                width="300"
+                alt="empty"
+                className="dark:hidden"
+            />
+            <Image 
+                src="/empty-dark.png"
+                height="300"
+                width="300"
+                alt="empty"
+                className="hidden dark:block"
+            />
         </div>
      );
 }
