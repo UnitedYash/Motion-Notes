627f5cb93487fc37cf0681f0b75002f9459db33e
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-01 14:10:53 -0400

    added schema user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import Image from "next/image";
-
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return ( 
         <div className="h-full flex flex-col items-center justify-center space-y-4">
             <Image 
@@ -19,6 +23,13 @@ const DocumentsPage = () => {
                 alt="empty"
                 className="hidden dark:block"
             />
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Motion
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a Note
+            </Button>
         </div>
      );
 }
