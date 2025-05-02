3fbc8fb03d5f77623ec3662e05911e607ae56fb9
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-02 12:55:01 -0400

    added onlclick document

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,14 +6,16 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const documents = useQuery(api.documents.get);
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sideBarRef = useRef<ElementRef<"aside">>(null);
@@ -91,7 +93,17 @@ export const Navigation = () => {
             setTimeout(() => setIsResetting(false), 300);
 
         }
-    }
+    };
+
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating new note",
+            success: "New note created",
+            error: "Failed to create new note"
+        });
+    };
 
     return (
         <>
@@ -112,7 +124,7 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
-                    <Item onClick={() => {}} label="New Page" icon={PlusCircle}/>
+                    <Item onClick={handleCreate} label="New Page" icon={PlusCircle}/>
                 </div>
                 <div className="mt-4">
                     {documents?.map((document) => (
