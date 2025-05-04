e716b73285560988e7417b874f7b853dcc0d1c6d
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-04 14:38:44 -0400

    added search func

--- hooks/use-search.tsx
+++ hooks/use-search.tsx
@@ -4,11 +4,12 @@ type SearchStore = {
     isOpen: boolean;
     onOpen: () => void;
     onClose: () => void;
+    toggle: () => void;
 }
 
 export const useSearch = create<SearchStore>((set, get) => ({
     isOpen: false,
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false}),
-    toggle: () => set({isOpen: !get().isOpen}),
+    toggle: () => set({isOpen:!get().isOpen})
 }));
