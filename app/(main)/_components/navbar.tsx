8f38eaa6e4acd10d6c988cff110f7d9e6c663208
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-07 16:48:02 -0400

    added dropdown

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -40,6 +41,9 @@ export const Navbar = ({
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && (
