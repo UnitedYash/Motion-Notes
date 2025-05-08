b52eda79a5bda3390593ffcf5113e08098ca452b
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-05-08 12:24:31 -0400

    added template cover image

---
+++ components/modals/cover-image-modal.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader
+} from "@/components/ui/dialog";
+import { useCoverImage } from "@/hooks/use-conver-image";
+
+export const CoverImageModal = () => {
+    const coverImage = useCoverImage();
+
+    return (
+        <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <h2 className="text-center text-lg font-semibold">
+                        Cover Image
+                    </h2>
+                </DialogHeader>
+                <div>
+                    TODO: Upload Image
+                </div>
+            </DialogContent>
+
+        </Dialog>
+    )
+};
+
+
